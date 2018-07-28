cea12ad65236fc87804bbe1f4835157ec207e98d
xiangkeguo <xiangkeguo@linkdoc.com>
2018-07-28 22:24:12 +0800

    test

--- web/modules/custom/lyapi/src/Controller/LyapiController.php
+++ web/modules/custom/lyapi/src/Controller/LyapiController.php
@@ -16,6 +16,7 @@ namespace Drupal\lyapi\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Drupal\taxonomy\Entity\Vocabulary;
 
 class LyapiController extends ControllerBase {
 	public function getResource($code) {
@@ -46,5 +47,30 @@ class LyapiController extends ControllerBase {
 		drupal_flush_all_caches();
 		\Drupal::logger('Cron clearCache')->notice(date('Ymd H:i:s'));
 		return new JsonResponse([]);
+	}
+	/**
+	 * 自动更新播放日期编号！
+	 */
+	public function getJson(){
+	  $vids = Vocabulary::loadMultiple();
+	  foreach ($vids as $vid) {
+	    // dsm($vid->label());
+	    if ($vid->label() == '良友节目') {
+
+	      $container = \Drupal::getContainer();
+	      $terms = $container->get('entity.manager')->getStorage('taxonomy_term')->loadTree($vid->id(), 0, NULL, TRUE);
+	      if (!empty($terms)) {
+	        foreach($terms as $term) {
+	          $code = $term->get('field_term_lylist_code')->value;
+	          // dpm($code);
+	          $data[$code]['day'] = $term->get('field_term_lylist_day')->value;
+	          $data[$code]['index'] = $term->get('field_term_lylist_index')->value;
+	          $data[$code]['ly_index'] = $term->get('field_term_lylist_lywx')->value;
+	        }
+	       return new JsonResponse(array_filter($data));
+	      }
+	      break;
+	    }
+	  }
 	}
 }
