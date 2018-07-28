5408f24cbcf6b4a37f01d58b424a63d6a756a2e9
xiangkeguo <xiangkeguo@linkdoc.com>
2018-07-28 22:38:47 +0800

    auto commit

--- web/modules/custom/lyapi/src/Controller/LyapiController.php
+++ web/modules/custom/lyapi/src/Controller/LyapiController.php
@@ -72,5 +72,29 @@ class LyapiController extends ControllerBase {
 	      break;
 	    }
 	  }
+	}/**
+	 * 自动更新播放日期编号！todo
+	 */
+	public function getLtsJson(){
+	  $vids = Vocabulary::loadMultiple();
+	  foreach ($vids as $vid) {
+	    // dsm($vid->label());
+	    if ($vid->label() == '良院课程') {
+
+	      $container = \Drupal::getContainer();
+	      $terms = $container->get('entity.manager')->getStorage('taxonomy_term')->loadTree($vid->id(), 0, NULL, TRUE);
+	      if (!empty($terms)) {
+	        foreach($terms as $term) {
+	          $code = $term->get('field_term_lylist_code')->value;
+	          // dpm($code);
+	          $data[$code]['day'] = $term->get('field_term_lylist_day')->value;
+	          $data[$code]['index'] = $term->get('field_term_lylist_index')->value;
+	          $data[$code]['ly_index'] = $term->get('field_term_lylist_lywx')->value;
+	        }
+	       return new JsonResponse(array_filter($data));
+	      }
+	      break;
+	    }
+	  }
 	}
 }
